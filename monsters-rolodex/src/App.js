b7308c8eac754ee47e361ef66347ffdb2d0be100
anvarbeyAze <anvarbey.creative@gmail.com>
2020-05-08 00:02:37 +0500

    react app

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,38 @@
+import React,{Component} from 'react';
+//import Component from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+     monsters: [
+       {
+         name:'Frankenstein',
+         id:'asc1'
+       },
+       {
+        name:'Dracula',
+        id:'asc2'
+      },
+      {
+        name:'Zombie',
+        id:'asc3'
+      }
+     ]
+    }
+  }
+  render(){
+
+  return (
+    <div className="App">
+     {
+       this.state.monsters.map(monster => (
+         <h1 key={monster.id}>{monster.name}</h1>
+         
+      ))}
+    </div>
+  );
+}}
+
+export default App;
