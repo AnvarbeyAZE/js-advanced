d3c01b82267e50a4ca41bf5cb88cd9061a709eed
anvarbeyAze <anvarbey.creative@gmail.com>
2020-05-12 00:09:54 +0500

    Updates

---
+++ precache-manifest.3cd48174c7192e1b09242bf002bd5ce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026f37a82f9f6d159d2008e9ac5838ad",
+    "url": "/js-advanced/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1e562eedd039fb1e4b22",
+    "url": "/js-advanced/monsters-rolodex/static/css/main.997e5d09.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/js-advanced/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-advanced/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e562eedd039fb1e4b22",
+    "url": "/js-advanced/monsters-rolodex/static/js/main.d375f23c.chunk.js"
+  },
+  {
+    "revision": "2d96b40239a9ddd1a6b0",
+    "url": "/js-advanced/monsters-rolodex/static/js/runtime-main.34f8fcdb.js"
+  }
+]);
